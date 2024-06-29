6c91d5b61556dc6c606c5aa8b82a17e1f82d38b6
yactouat <yacine.touati.pro@gmail.com>
2024-06-29 18:28:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ypostgres_lib
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ypostgres_lib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here if any
+    ],
+    author='yactouat',
+    author_email='yacine.touati.pro@gmail.com',
+    description='helper functions when I talk to Postgres directly',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yactouat/ypostgres_lib',
+    license='MIT',
+)
