60c6048af9de30ded4760700f5c511fea75f2db9
yactouat <yactouat@yactouat.com>
2024-09-08 08:34:23 +0200

    2.4.0

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ypostgres_lib',
-    version='2.2.0',
+    name="ypostgres_lib",
+    version="2.4.0",
     packages=find_packages(),
-    install_requires=[
-        # List your dependencies here if any
-    ],
-    author='yactouat',
-    author_email='yactouat@yactouat.com',
-    description='helper functions for when I need talk to Postgres',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yactouat/ypostgres_lib',
-    license='MIT',
+    install_requires=["psycopg2-binary"],
+    author="yactouat",
+    author_email="yactouat@yactouat.com",
+    description="helper functions for when I need talk to Postgres",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yactouat/ypostgres_lib",
+    license="MIT",
 )
--- ypostgres_lib/postgres_lib.py
+++ ypostgres_lib/postgres_lib.py
@@ -2,7 +2,7 @@ import psycopg2
 import os
 
 
-def close_conn(conn, cur):
+def close_conn(conn, cur) -> None:
     """
     Close the connection to the database.
     """
@@ -26,20 +26,61 @@ def get_conn():
     )
 
 
-def run_static_dml(conn, query: str):
+def run_paginated_parameterized_dql(
+    query: str, params: tuple, offset: int, limit: int
+) -> list:
     """
-    Run a static DML statement without getting the results.
+    Run a paginated DQL statement and get the results.
+
+    Args:
+        query (str): The SQL query to execute (without OFFSET and LIMIT).
+        params (tuple): The parameters for the query.
+        offset (int): The number of rows to skip. The first row has an offset of 0.
+        limit (int): The maximum number of rows to return.
+
+    Returns:
+        list: The paginated query results.
+
+    Note:
+        - The lower boundary (offset) is inclusive.
+        - The upper boundary (offset + limit) is exclusive.
+        - For example, with offset=10 and limit=5, you get rows 10, 11, 12, 13, and 14.
     """
+    paginated_query = f"{query} OFFSET %s LIMIT %s"
+    paginated_params = params + (offset, limit)
+
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute(query)
+
+    cur.execute(paginated_query, paginated_params)
+    results = cur.fetchall()
+
+    close_conn(conn, cur)
+
+    return results
+
+
+def run_parameterized_item_dml(
+    statement: str, params: tuple, with_results: bool = False
+) -> list | None:
+    """
+    Run a parametrized DML SQL query.
+    You can choose to return the results of the query.
+    """
+    results = None
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute(statement, params)
+    if with_results:
+        results = cur.fetchall()
     conn.commit()
 
-    cur.close()
-    conn.close()
+    close_conn(conn, cur)
+    return results
 
 
-def run_parameterized_dql(query: str, params: tuple):
+def run_parameterized_dql(query: str, params: tuple) -> list:
     """
     Run a parameterized DQL statement and get the results.
     """
@@ -54,7 +95,20 @@ def run_parameterized_dql(query: str, params: tuple):
     return results
 
 
-def run_static_dql(query: str):
+def run_static_dml(conn, query: str) -> None:
+    """
+    Run a static DML statement without getting the results.
+    """
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute(query)
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+def run_static_dql(query: str) -> list:
     """
     Run a static DQL statement and get the results.
     """
